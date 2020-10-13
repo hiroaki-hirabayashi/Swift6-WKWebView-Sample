fb25311e52e6b152766b7fa30fbe35ce29ef3c1c
Hiroki Hirabayashi <hh.38063806@gmail.com>
2020-10-13 18:14:59 +0900

    Initial Commit

---
+++ Swift6 WKWebView Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift6 WKWebView Sample
+//
+//  Created by 平林宏淳 on 2020/10/13.
+//  Copyright © 2020 Hiroaki_Hirabayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
