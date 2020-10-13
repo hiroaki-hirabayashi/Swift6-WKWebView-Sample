2f446c83607a561c355fafe3afb6dd4e1b2db32c
Hiroki Hirabayashi <hh.38063806@gmail.com>
2020-10-14 02:47:05 +0900

    ブラウザを表示出来る様に変更

--- Swift6 WKWebView Sample/ViewController.swift
+++ Swift6 WKWebView Sample/ViewController.swift
@@ -9,32 +9,60 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController, WKNavigationDelegate {
+class ViewController: UIViewController, WKNavigationDelegate, UISearchBarDelegate {
 
     @IBOutlet weak var indicator: UIActivityIndicatorView!
     @IBOutlet weak var toolBar: UIToolbar!
+    @IBOutlet weak var seachBar: UISearchBar!
     
     var webView = WKWebView()
     
+    // ホームページのURL。起動時にこのページを開く
+    let homeUrl = "https://www.google.com/?hl=ja"
+    // 検索機能で使うURL
+    let searchUrl = "https://www.google.co.jp/search?q="
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setUpWebView()
-        
+        webView.navigationDelegate = self
+        openUrl(urlString: homeUrl)
+
     }
     
-    func setUpWebView() {
+    // 文字列で指定されたURLをWeb Viewを開く
+    func openUrl(urlString: String) {
         indicator.isHidden = true
         webView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height - toolBar.frame.size.height)
         view.addSubview(webView)
         
-        webView.navigationDelegate = self
-        let url = URL(string: "https://www.kurashiru.com/")
+        
+        let url = URL(string: urlString)
         let request = URLRequest(url: url!)
         webView.load(request)
         
         indicator.layer.zPosition = 2
+
     }
     
+    // 検索ボタンを押下した時に実行されるメソッド
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        if let searchText = searchBar.text {
+            let url = searchUrl + searchText.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed)!
+            openUrl(urlString: url)
+            searchBar.resignFirstResponder()
+        }
+    }
+
+    
+    func setUpWebView() {
+        
+//        let url = URL(string: "https://www.kurashiru.com/")
+//        let request = URLRequest(url: url!)
+//        webView.load(request)
+//
+    }
+    
+        // Web Viewが読み込みを終了した時に実行されるメソッド
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         indicator.isHidden = true
         indicator.stopAnimating()
@@ -44,19 +72,16 @@ class ViewController: UIViewController, WKNavigationDelegate {
         indicator.isHidden = false
         indicator.startAnimating()
     }
-
-    @IBAction func backButton(_ sender: Any) {
+        
+    @IBAction func backButtonTapped(_ sender: Any) {
         webView.goBack()
     }
     
-    @IBAction func goButton(_ sender: Any) {
+    @IBAction func forwardButtonTapped(_ sender: Any) {
         webView.goForward()
     }
-    
-//extension WKNavigationDelegate {
-////    webView.navigationDelegate = self
-//
-//
-//  }
+   
+    @IBAction func reloadButtonTapped(_ sender: Any) {
+        webView.reload()
+    }
 }
-
